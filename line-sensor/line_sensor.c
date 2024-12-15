c2da4e7fc752e638390460cb6a9aea0d5bbb6def
YuvrajGupta1808 <ygupta@sfsu.edu>
2024-12-15 11:32:19 -0800

    expand to 5 sensors

--- line-sensor/line_sensor.c
+++ line-sensor/line_sensor.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 
 // Array to store sensor readings
-int sensor_readings[MAX_READINGS][3];
+int sensor_readings[MAX_READINGS][NUM_SENSORS];
 int reading_index = 0;
 
 // Initialize the line sensors GPIO pins
@@ -17,16 +17,20 @@ void line_sensors_init() {
     }
 
     // Set the sensor pins as input
+    bcm2835_gpio_fsel(SENSOR_0_PIN, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_fsel(SENSOR_1_PIN, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_fsel(SENSOR_2_PIN, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_fsel(SENSOR_3_PIN, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_fsel(SENSOR_4_PIN, BCM2835_GPIO_FSEL_INPT);
 }
 
 // Read the state of all line sensors and store in an array
 void read_line_sensors(int* sensor_states) {
-    sensor_states[0] = bcm2835_gpio_lev(SENSOR_1_PIN);
-    sensor_states[1] = bcm2835_gpio_lev(SENSOR_2_PIN);
-    sensor_states[2] = bcm2835_gpio_lev(SENSOR_3_PIN);
+    sensor_states[0] = bcm2835_gpio_lev(SENSOR_0_PIN);
+    sensor_states[1] = bcm2835_gpio_lev(SENSOR_1_PIN);
+    sensor_states[2] = bcm2835_gpio_lev(SENSOR_2_PIN);
+    sensor_states[3] = bcm2835_gpio_lev(SENSOR_3_PIN);
+    sensor_states[4] = bcm2835_gpio_lev(SENSOR_4_PIN);
 }
 
 // Test the line sensors and store results in an array
@@ -49,10 +53,11 @@ void test_line_sensors() {
         }
 
         // Print the states of the line sensors as 1 or 0
-        printf("Reading #%d: %d %d %d\n", reading_index,
-               sensor_readings[reading_index - 1][0],
-               sensor_readings[reading_index - 1][1],
-               sensor_readings[reading_index - 1][2]);
+        printf("Reading #%d:", reading_index);
+        for (int i = 0; i < NUM_SENSORS; i++) {
+            printf(" %d", sensor_readings[reading_index - 1][i]);
+        }
+        printf("\n");
 
         fflush(stdout);  // Ensure output is displayed immediately
         usleep(50000);   // Sleep for 50ms
